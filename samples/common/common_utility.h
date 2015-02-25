87505524acf0d7657575fe2c8f21bef314350cb1
Ding, Wei (xN/A) TX <Wei.Ding2@amd.com>
2015-02-25 13:05:59 -0500

    ECR #333755 - Added two HSA samples: MatrixMultiplication, MatrixTranspose
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/runtime/": change = 1124951]
    
    
    [ROCm/ROCR-Runtime commit: 9d9ea2ad3b3bdcc9b6b9871f19b2ed3aa166d54a]

---
+++ samples/common/common_utility.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+double CalcMean(vector<double> scores);
+double CalcMedian(vector<double> scores);
+double CalcStdDeviation(vector<double> scores, int score_mean);
+int CalcConcurrentQueues(vector<double> scores);
+
+
+
