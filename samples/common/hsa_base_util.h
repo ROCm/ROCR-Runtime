8863254f76103dabf2cfd804728d44e1c2cdd2d9
Ding, Wei (xN/A) TX <Wei.Ding2@amd.com>
2015-04-07 16:16:06 -0500

    ECR #333755 - Code clean up for releasing.
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/runtime/": change = 1138207]

--- samples/common/hsa_base_util.h
+++ samples/common/hsa_base_util.h
@@ -1,7 +1,6 @@
 #ifndef __HSA_UTIL__
 #define __HSA_UTIL__
 
-
 #include <vector>
 #include "hsa.h"
 #include "hsa_ext_finalize.h"
@@ -11,8 +10,6 @@
 #include "assemble.hpp"
 #include "common.hpp"
 
-
-
 class HSA_UTIL{
     public:
 	    HSA_UTIL();
@@ -21,8 +18,8 @@ class HSA_UTIL{
 	public:
 	    void GetHsailNameAndKernelName(char *hail_file_name, char *kernel_name);
 	    bool HsaInit();
-        void Close();
-	double Run(int dim, int group_x, int group_y, int group_z, int s_size, int grid_x, int grid_y, int grid_z, void* kernel_args, int kernel_args_size);
+            void Close();
+	    double Run(int dim, int group_x, int group_y, int group_z, int s_size, int grid_x, int grid_y, int grid_z, void* kernel_args, int kernel_args_size);
 
 	public:
 		hsa_status_t err;
@@ -34,10 +31,6 @@ class HSA_UTIL{
 
 		hsa_queue_t* command_queue;
 		hsa_signal_t hsa_signal;
-		//hsa_ext_brig_module_t* brig_module;
-		//hsa_ext_brig_module_handle_t module;
-		//hsa_ext_program_handle_t hsa_program;
-		//hsa_ext_code_descriptor_t *hsa_code_descriptor;
 		hsa_kernel_dispatch_packet_t dispatch_packet; // needs to be set manually each time	
 		hsa_region_t hsa_kernarg_region;
 };
