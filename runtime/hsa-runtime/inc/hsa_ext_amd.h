7760839934ef0fd53e2495cacdd84bbdeeb4c6fe
Besar Wicaksono (xN/A) TX [TEXT] <besar.wicaksono@amd.com>
2016-04-08 16:40:05 -0500

    Fix build error from CL#1256102 due to whitespace issue.
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/runtime/": change = 1256108]

--- runtime/hsa-runtime/inc/hsa_ext_amd.h
+++ runtime/hsa-runtime/inc/hsa_ext_amd.h
@@ -49,7 +49,7 @@
 #include "hsa_ext_image.h"
 
 #define HSA_AMD_INTERFACE_VERSION_MAJOR 1
-#define HSA_AMD_INTERFACE_VERSION_MINOR 0 
+#define HSA_AMD_INTERFACE_VERSION_MINOR 0
 
 #ifdef __cplusplus
 extern "C" {
