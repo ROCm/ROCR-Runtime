2ebde5d2a7fd3569058f16861b7c74f4bfa29808
Besar Wicaksono (xN/A) TX [TEXT] <besar.wicaksono@amd.com>
2016-04-08 16:51:45 -0500

    Fix unit test build error due to CL#1256098
    [git-p4: depot-paths = "//depot/stg/hsa/drivers/hsa/runtime/": change = 1256119]

--- runtime/hsa-runtime/inc/hsa_ext_amd.h
+++ runtime/hsa-runtime/inc/hsa_ext_amd.h
@@ -48,8 +48,8 @@
 #include "hsa.h"
 #include "hsa_ext_image.h"
 
-#define HSA_AMD_INTERFACE_VERSION_MAJOR 1
-#define HSA_AMD_INTERFACE_VERSION_MINOR 0
+#define HSA_AMD_INTERFACE_VERSION_MAJOR 1
+#define HSA_AMD_INTERFACE_VERSION_MINOR 0
 
 #ifdef __cplusplus
 extern "C" {
