bc44715be2b810da5fe7c3e83c8b43dda4877f44
James Edwards <JamesAdrian.Edwards@amd.com>
2017-02-08 14:39:15 -0600

    Update libhsakmt CMakeList.txt file for tag based versioning and proper packaging
    Change-Id: I63e82deefa8377ced810d99b5b2f0457299048a6

--- cmake_modules/utils.cmake
+++ cmake_modules/utils.cmake
@@ -3,7 +3,7 @@
 ## The University of Illinois/NCSA
 ## Open Source License (NCSA)
 ##
-## Copyright (c) 2014-2015, Advanced Micro Devices, Inc. All rights reserved.
+## Copyright (c) 2014-2017, Advanced Micro Devices, Inc. All rights reserved.
 ##
 ## Developed by:
 ##
@@ -41,31 +41,76 @@
 ################################################################################
 
 ## Parses the VERSION_STRING variable and places
-## the first, second and third number values in 
+## the first, second and third number values in
 ## the major, minor and patch variables.
-function(parse_version VERSION_STRING)
+function( parse_version VERSION_STRING )
 
-    string(REGEX MATCHALL "[0123456789]+" VERSIONS ${VERSION_STRING})
-    list(LENGTH VERSIONS VERSION_COUNT)
+    string ( FIND ${VERSION_STRING} "-" STRING_INDEX )
 
-    if (${VERSION_COUNT} GREATER 0) 
-        list(GET VERSIONS 0 MAJOR)
-        set(VERSION_MAJOR ${MAJOR} PARENT_SCOPE)
-        set(TEMP_VERSION_STRING "${MAJOR}")
+    if ( ${STRING_INDEX} GREATER -1 )
+        math ( EXPR STRING_INDEX "${STRING_INDEX} + 1" )
+        string ( SUBSTRING ${VERSION_STRING} ${STRING_INDEX} -1 VERSION_BUILD )
     endif ()
 
-    if (${VERSION_COUNT} GREATER 1) 
-        list(GET VERSIONS 1 MINOR)
-        set(VERSION_MINOR ${MINOR} PARENT_SCOPE)
-        set(TEMP_VERSION_STRING "${TEMP_VERSION_STRING}.${MINOR}")
+    string ( REGEX MATCHALL "[0123456789]+" VERSIONS ${VERSION_STRING} )
+    list (LENGTH VERSIONS VERSION_COUNT )
+
+    if ( ${VERSION_COUNT} GREATER 0)
+        list ( GET VERSIONS 0 MAJOR )
+        set ( VERSION_MAJOR ${MAJOR} PARENT_SCOPE )
+        set ( TEMP_VERSION_STRING "${MAJOR}" )
     endif ()
 
-    if (${VERSION_COUNT} GREATER 2) 
-        list(GET VERSIONS 2 PATCH)
-        set(VERSION_PATCH ${PATCH} PARENT_SCOPE)
-        set(TEMP_VERSION_STRING "${TEMP_VERSION_STRING}.${PATCH}")
+    if ( ${VERSION_COUNT} GREATER 1 )
+        list ( GET VERSIONS 1 MINOR )
+        set ( VERSION_MINOR ${MINOR} PARENT_SCOPE )
+        set ( TEMP_VERSION_STRING "${TEMP_VERSION_STRING}.${MINOR}" )
     endif ()
 
-    set(VERSION_STRING "${TEMP_VERSION_STRING}" PARENT_SCOPE)
+    if ( ${VERSION_COUNT} GREATER 2 )
+        list ( GET VERSIONS 2 PATCH )
+        set ( VERSION_PATCH ${PATCH} PARENT_SCOPE )
+        set ( TEMP_VERSION_STRING "${TEMP_VERSION_STRING}.${PATCH}" )
+    endif ()
+
+    if ( DEFINED VERSION_BUILD )
+        set ( VERSION_BUILD "${VERSION_BUILD}" PARENT_SCOPE )
+    endif ()
+
+    set ( VERSION_STRING "${TEMP_VERSION_STRING}" PARENT_SCOPE )
+
+endfunction ()
+
+## Gets the current version of the repository
+## using versioning tags and git describe.
+## Passes back a packaging version string
+## and a library version string.
+function ( get_version DEFAULT_VERSION_STRING )
+
+    parse_version ( ${DEFAULT_VERSION_STRING} )
+
+    find_program ( GIT NAMES git )
+
+    if ( GIT )
+
+        execute_process ( COMMAND git describe --long --match [0-9]*
+                          WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                          OUTPUT_VARIABLE GIT_TAG_STRING
+                          OUTPUT_STRIP_TRAILING_WHITESPACE
+                          RESULT_VARIABLE RESULT )
+
+        if ( ${RESULT} EQUAL 0 )
+
+            parse_version ( ${GIT_TAG_STRING} )
+
+        endif ()
+
+    endif ()
+
+    set( VERSION_STRING "${VERSION_STRING}" PARENT_SCOPE )
+    set( VERSION_MAJOR  "${VERSION_MAJOR}" PARENT_SCOPE )
+    set( VERSION_MINOR  "${VERSION_MINOR}" PARENT_SCOPE )
+    set( VERSION_PATCH  "${VERSION_PATCH}" PARENT_SCOPE )
+    set( VERSION_BUILD  "${VERSION_BUILD}" PARENT_SCOPE )
 
 endfunction()
