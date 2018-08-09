6c4778062048dda981919e198c5f22ab2fd05121
Sean Keely <Sean.Keely@amd.com>
2018-08-08 20:15:22 -0500

    Experimental flag to swap copy agent for async copy APIs.
    Adds env flag HSA_REV_COPY_DIR.  If set to 1 async copy will
    copy from dst device to src device rather than from src to dst.
    
    Change-Id: I3095642066fa026dc112c2eac06db9393341cd7e

--- runtime/hsa-runtime/core/runtime/runtime.cpp
+++ runtime/hsa-runtime/core/runtime/runtime.cpp
@@ -416,9 +416,11 @@ hsa_status_t Runtime::CopyMemory(void* dst, core::Agent& dst_agent,
   const bool src_gpu =
       (src_agent.device_type() == core::Agent::DeviceType::kAmdGpuDevice);
   if (dst_gpu || src_gpu) {
-    core::Agent& copy_agent = (src_gpu) ? src_agent : dst_agent;
-    return copy_agent.DmaCopy(dst, dst_agent, src, src_agent, size, dep_signals,
-                              completion_signal);
+    core::Agent* copy_agent = (src_gpu) ? &src_agent : &dst_agent;
+    if (flag_.rev_copy_dir() && dst_gpu && src_gpu)
+      copy_agent = (copy_agent == &src_agent) ? &dst_agent : &src_agent;
+    return copy_agent->DmaCopy(dst, dst_agent, src, src_agent, size, dep_signals,
+                               completion_signal);
   }
 
   // For cpu to cpu, fire and forget a copy thread.
