1d2d44c122654c39336eb1babfc77ccaa50d99b4
Junwei Zhang <zjunweihit@163.com>
2019-09-25 15:22:18 +0800

    pull data from each section in a loop for elf image
    simplify the code to do pull all data in a loop,
    that will call each type section correspondingly.

--- src/libamdhsacode/amd_elf_image.cpp
+++ src/libamdhsacode/amd_elf_image.cpp
@@ -1408,23 +1408,7 @@ namespace amd {
 
       for (size_t n = 1; n < sections.size(); ++n) {
         GElfSection* section = sections[n].get();
-        if (section->type() == SHT_STRTAB) {
-          if (!section->pullData()) { return false; }
-        }
-      }
-
-      for (size_t n = 1; n < sections.size(); ++n) {
-        GElfSection* section = sections[n].get();
-        if (section->type() == SHT_SYMTAB || section->type() == SHT_DYNSYM) {
-          if (!section->pullData()) { return false; }
-        }
-      }
-
-      for (size_t n = 1; n < sections.size(); ++n) {
-        GElfSection* section = sections[n].get();
-        if (section->type() != SHT_STRTAB && section->type() != SHT_SYMTAB && section->type() != SHT_DYNSYM) {
-          if (!section->pullData()) { return false; }
-        }
+        if (!section->pullData()) { return false; }
       }
 
       for (size_t i = 1; i < sections.size(); ++i) {
