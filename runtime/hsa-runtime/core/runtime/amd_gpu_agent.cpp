ddebda6433b920625a5e656012beabcd4b8388cf
Sean Keely <Sean.Keely@amd.com>
2019-12-20 16:42:52 -0600

    Add USER GCR packet
    Allows cache flush from SDMA.
    
    Change-Id: Iecae0b49519b2d4da7b1c053b03f721544439e71

--- runtime/hsa-runtime/core/runtime/amd_gpu_agent.cpp
+++ runtime/hsa-runtime/core/runtime/amd_gpu_agent.cpp
@@ -488,16 +488,26 @@ core::Queue* GpuAgent::CreateInterceptibleQueue() {
 core::Blit* GpuAgent::CreateBlitSdma(bool use_xgmi) {
   amd::BlitSdmaBase* sdma;
 
-  if (isa_->GetMajorVersion() <= 8) {
-    sdma = new BlitSdmaV2V3();
-  } else {
-    sdma = new BlitSdmaV4();
+  switch (isa_->GetMajorVersion()) {
+    case 7:
+    case 8:
+      sdma = new BlitSdmaV2V3();
+      break;
+    case 9:
+      sdma = new BlitSdmaV4();
+      break;
+    case 10:
+      sdma = new BlitSdmaV5();
+      break;
+    default:
+      assert(false && "Unexpected device major version.");
+      return nullptr;
   }
 
   if (sdma->Initialize(*this, use_xgmi) != HSA_STATUS_SUCCESS) {
     sdma->Destroy(*this);
     delete sdma;
-    sdma = NULL;
+    sdma = nullptr;
   }
 
   return sdma;
@@ -533,7 +543,8 @@ void GpuAgent::InitDma() {
   auto blit_lambda = [this](bool use_xgmi, lazy_ptr<core::Queue>& queue) {
     const std::string& sdma_override = core::Runtime::runtime_singleton_->flag().enable_sdma();
 
-    bool use_sdma = ((isa_->GetMajorVersion() != 8) && (isa_->GetMajorVersion() != 10));
+    // User SDMA queues are unstable on gfx8.
+    bool use_sdma = ((isa_->GetMajorVersion() != 8));
     if (sdma_override.size() != 0) use_sdma = (sdma_override == "1");
 
     if (use_sdma && (HSA_PROFILE_BASE == profile_)) {
