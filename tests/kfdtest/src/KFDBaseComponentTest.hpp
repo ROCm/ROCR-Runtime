235636d59856e08f9e49fbec0015bc4652760e9c
Graham Sider <Graham.Sider@amd.com>
2021-10-08 13:35:51 -0400

    kfdtest: Instantiate Assembler in KFDBaseComponentTest
    Instantiate in KFDBaseComponentTest::SetUp() and destroy in TearDown().
    This ensures m_pAsm is available for all tests.
    
    Signed-off-by: Graham Sider <Graham.Sider@amd.com>
    Change-Id: I8b98a5350a9739d71455f14552c9879bdb1c475d

--- tests/kfdtest/src/KFDBaseComponentTest.hpp
+++ tests/kfdtest/src/KFDBaseComponentTest.hpp
@@ -34,6 +34,7 @@
 #include "hsakmt.h"
 #include "OSWrapper.hpp"
 #include "KFDTestUtil.hpp"
+#include "Assemble.hpp"
 
 //  @class KFDBaseComponentTest
 class KFDBaseComponentTest : public testing::Test {
@@ -74,6 +75,7 @@ class KFDBaseComponentTest : public testing::Test {
     HsaMemFlags m_MemoryFlags;
     HsaNodeInfo m_NodeInfo;
     HSAint32 m_xnack;
+    Assembler* m_pAsm;
 
     // @brief Executed before every test that uses KFDBaseComponentTest class and sets all common settings for the tests.
     virtual void SetUp();
