54c8b7900d0859ed0eee856a674fff71cdf76b34
Yifan Zhang <yifan1.zhang@amd.com>
2021-11-18 16:32:20 +0800

    add gfx1036 support
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Change-Id: Ifc1b3cf2e46cf753f57470ebc6b034c1a349d3d2

--- runtime/hsa-runtime/image/blit_kernel.cpp
+++ runtime/hsa-runtime/image/blit_kernel.cpp
@@ -95,6 +95,7 @@ extern uint8_t ocl_blit_object_gfx1032[];
 extern uint8_t ocl_blit_object_gfx1033[];
 extern uint8_t ocl_blit_object_gfx1034[];
 extern uint8_t ocl_blit_object_gfx1035[];
+extern uint8_t ocl_blit_object_gfx1036[];
 
 // Arguments inserted by OCL compiler, all zero here.
 struct OCLHiddenArgs {
@@ -1022,6 +1023,8 @@ hsa_status_t BlitKernel::GetPatchedBlitObject(const char* agent_name,
     *blit_code_object = ocl_blit_object_gfx1034;
   } else if (sname == "gfx1035") {
     *blit_code_object = ocl_blit_object_gfx1035;
+  } else if (sname == "gfx1036") {
+    *blit_code_object = ocl_blit_object_gfx1036;
   } else {
     return HSA_STATUS_ERROR_INVALID_ISA_NAME;
   }
