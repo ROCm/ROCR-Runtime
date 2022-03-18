7e73760cd05db5c95db120dc97cf31350a3215e2
Sean Keely <Sean.Keely@amd.com>
2022-03-18 02:33:36 -0500

    Revert "add gfx1036 support"
    Compiler is not promoted to mainline yet.
    
    This reverts commit 2f97f17df9c7682d6c914a75f48fc5457aab6b0b.
    
    Change-Id: I7256aeb3698ee3ae640a9f457a929abe24d5ef17

--- runtime/hsa-runtime/image/device_info.cpp
+++ runtime/hsa-runtime/image/device_info.cpp
@@ -188,7 +188,6 @@ uint32_t DevIDToAddrLibFamily(uint32_t dev_id) {
               case 3:
               case 4:
               case 5:
-              case 6:
                 return FAMILY_NV;
 
               default:
