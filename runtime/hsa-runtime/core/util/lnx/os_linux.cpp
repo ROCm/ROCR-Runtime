df3fe8c2fb9754ee472d92c5e841b893abab833f
David Yat Sin <David.YatSin@amd.com>
2022-08-17 11:31:39 -0400

    Add env variable to disable CPU affinity override
    New environment variable HSA_OVERRIDE_CPU_AFFINITY_DEBUG to
    enable/disable overriding CPU affinity.
    
    Default value is enabled(1).
    
    This is a temporary variable and may be removed in the future.
    
    Change-Id: Id6a7c611730471ddc276ca333fde1e57046bf32a

--- runtime/hsa-runtime/core/util/lnx/os_linux.cpp
+++ runtime/hsa-runtime/core/util/lnx/os_linux.cpp
@@ -59,6 +59,7 @@
 #include <memory>
 #include <string>
 #include <utility>
+#include "core/inc/runtime.h"
 
 namespace rocr {
 namespace os {
@@ -99,16 +100,18 @@ class os_thread {
       assert(err == 0 && "pthread_attr_setstacksize failed.");
     }
 
-    int cores = get_nprocs_conf();
-    cpu_set_t* cpuset = CPU_ALLOC(cores);
-    for(int i=0; i<cores; i++){
-      CPU_SET(i, cpuset);
+    if (core::Runtime::runtime_singleton_->flag().override_cpu_affinity()) {
+      int cores = get_nprocs_conf();
+      cpu_set_t* cpuset = CPU_ALLOC(cores);
+      for (int i = 0; i < cores; i++) {
+        CPU_SET(i, cpuset);
+      }
+      int err = pthread_attr_setaffinity_np(&attrib, CPU_ALLOC_SIZE(cores), cpuset);
+      assert(err == 0 && "pthread_attr_setaffinity_np failed.");
+      CPU_FREE(cpuset);
     }
-    int err = pthread_attr_setaffinity_np(&attrib, CPU_ALLOC_SIZE(cores), cpuset);
-    assert(err == 0 && "pthread_attr_setaffinity_np failed.");
-    CPU_FREE(cpuset);
 
-    err = pthread_create(&thread, &attrib, ThreadTrampoline, args.get());
+    int err = pthread_create(&thread, &attrib, ThreadTrampoline, args.get());
 
     // Probably a stack size error since system limits can be different from PTHREAD_STACK_MIN
     // Attempt to grow the stack within reason.
