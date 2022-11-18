01fd84db5e11102e4b0e9ee6ac145cd7f2f663c0
Ranjith Ramakrishnan <Ranjith.Ramakrishnan@amd.com>
2022-11-17 17:14:41 -0800

    Change pragma message to warning
    File reorganization feature was implemented with backward compatibility
    The backward compatibility support will be deprecated in future release.
    Changed the #pragma message to #warning for a smooth transition
    
    Change-Id: I21025f4cefb40721f095130263b4247877979d36

--- hsakmt-backward-compat.cmake
+++ hsakmt-backward-compat.cmake
@@ -41,7 +41,21 @@ function(create_header_template)
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
    THE SOFTWARE.
-   */\n\n#ifndef @include_guard@\n#define @include_guard@ \n\n#pragma message(\"@file_name@ has moved to @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@/hsakmt and package include paths have changed.\\nInclude as \\\"hsakmt/@file_name@\\\" when using cmake packages.\")\n@include_statements@\n\n#endif")
+   */
+
+#ifndef @include_guard@
+#define @include_guard@
+
+
+#if defined(__GNUC__)
+#warning \"@file_name@ has moved to @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@/hsakmt and package include paths have changed.\\nInclude as \\\"hsakmt/@file_name@\\\" when using cmake packages.\"
+#else
+#pragma message(\"@file_name@ has moved to @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@/hsakmt and package include paths have changed.\\nInclude as \\\"hsakmt/@file_name@\\\" when using cmake packages.\")
+#endif
+
+@include_statements@
+
+#endif")
 endfunction()
 
 #use header template file and generate wrapper header files
