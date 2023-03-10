629ddde072dd474087ecea7f4398b475de500d0d
Ranjith Ramakrishnan <Ranjith.Ramakrishnan@amd.com>
2023-03-10 08:38:09 -0800

    Compile time flag to switch between #warning and #error message
    Using backward compatibility paths will provide an #error message. Compile time option added to enable/disable the #error message.
    Disabling the same will provide a #warning message
    
    Change-Id: Ib48e361b72176e2845c8f74f980f0234e7eb4a7d

--- runtime/hsa-runtime/hsa-backward-compat.cmake
+++ runtime/hsa-runtime/hsa-backward-compat.cmake
@@ -48,7 +48,18 @@ function(create_header_template)
 #ifndef @include_guard@
 #define @include_guard@
 
+#ifndef ROCM_HEADER_WRAPPER_WERROR
+#define ROCM_HEADER_WRAPPER_WERROR @deprecated_error@
+#endif
+#if ROCM_HEADER_WRAPPER_WERROR  /* ROCM_HEADER_WRAPPER_WERROR 1 */
 #error \"@file_name@ has moved to @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@/hsa and package include paths have changed.\\nInclude as \\\"hsa/@file_name@\\\" when using cmake packages.\"
+#else  /* ROCM_HEADER_WRAPPER_WERROR 0 */
+#if defined(__GNUC__)
+#warning \"@file_name@ has moved to @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@/hsa and package include paths have changed.\\nInclude as \\\"hsa/@file_name@\\\" when using cmake packages.\"
+#else
+#pragma message(\"@file_name@ has moved to @CMAKE_INSTALL_PREFIX@/@CMAKE_INSTALL_INCLUDEDIR@/hsa and package include paths have changed.\\nInclude as \\\"hsa/@file_name@\\\" when using cmake packages.\")
+#endif
+#endif  /* ROCM_HEADER_WRAPPER_WERROR */
 @include_statements@
 
 #endif")
