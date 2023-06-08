f36e6711af8bb59d7658ec17bc13c9b36ff98cc8
Ammar ELWazir <Ammar.ELWazir@amd.com>
2023-06-08 21:21:59 +0000

    : Adding support to UMC & MMEA System Blocks
    Change-Id: I92601f37757e0cff3f1fdc10f2e5e0db51c1ee2d
    
    
    [ROCm/ROCR-Runtime commit: fc603d58d2e0ced5c49a1c644fc1e6ab72617a07]

--- runtime/hsa-runtime/inc/hsa_ven_amd_aqlprofile.h
+++ runtime/hsa-runtime/inc/hsa_ven_amd_aqlprofile.h
@@ -2,24 +2,24 @@
 //
 // The University of Illinois/NCSA
 // Open Source License (NCSA)
-// 
+//
 // Copyright (c) 2017-2020, Advanced Micro Devices, Inc. All rights reserved.
-// 
+//
 // Developed by:
-// 
+//
 //                 AMD Research and AMD HSA Software Development
-// 
+//
 //                 Advanced Micro Devices, Inc.
-// 
+//
 //                 www.amd.com
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
 // deal with the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
-// 
+//
 //  - Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimers.
 //  - Redistributions in binary form must reproduce the above copyright
@@ -29,7 +29,7 @@
 //    nor the names of its contributors may be used to endorse or promote
 //    products derived from this Software without specific prior written
 //    permission.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -121,6 +121,10 @@ typedef enum {
   HSA_VEN_AMD_AQLPROFILE_BLOCK_NAME_GL2C = 29,
   HSA_VEN_AMD_AQLPROFILE_BLOCK_NAME_GCR = 30,
   HSA_VEN_AMD_AQLPROFILE_BLOCK_NAME_GUS = 31,
+
+  // UMC & MMEA System Blocks
+  HSA_VEN_AMD_AQLPROFILE_BLOCK_NAME_UMC = 32,
+  HSA_VEN_AMD_AQLPROFILE_BLOCK_NAME_MMEA = 33,
 
   HSA_VEN_AMD_AQLPROFILE_BLOCKS_NUMBER
 } hsa_ven_amd_aqlprofile_block_name_t;
