0138de36b78f1e408053e9ff551baa9d0a6b7260
David Yat Sin <David.YatSin@amd.com>
2023-08-22 23:50:38 +0000

    PC Sampling: Retrieve data from trap handler
    Retrieve data from the buffers previously set in the 2nd level trap
    handler TMA. We use a double buffering mechanism to allow the 2nd level
    trap handler to write to one buffer while we are copying data from the
    other.
    
    Co-authored by: Joseph Greathouse <Joseph.Greathouse@amd.com>
    Co-authored by: James Zhu <James.Zhu@amd.com>
    
    Change-Id: I252c381ea06b8cf927c4f9af6ea59dedc3717fbb
    
    
    [ROCm/ROCR-Runtime commit: 855e454671bcc1bc0c7b95416639ea8769c73803]

--- runtime/hsa-runtime/core/inc/amd_gpu_pm4.h
+++ runtime/hsa-runtime/core/inc/amd_gpu_pm4.h
@@ -43,10 +43,18 @@
 #ifndef HSA_RUNTIME_CORE_INC_AMD_GPU_PM4_H_
 #define HSA_RUNTIME_CORE_INC_AMD_GPU_PM4_H_
 
+ // clang-format off
+
 #define PM4_HDR_IT_OPCODE_NOP                             0x10
 #define PM4_HDR_IT_OPCODE_INDIRECT_BUFFER                 0x3F
 #define PM4_HDR_IT_OPCODE_RELEASE_MEM                     0x49
 #define PM4_HDR_IT_OPCODE_ACQUIRE_MEM                     0x58
+
+#define PM4_HDR_IT_OPCODE_ATOMIC_MEM                      0x1E
+#define PM4_HDR_IT_OPCODE_WRITE_DATA                      0x37
+#define PM4_HDR_IT_OPCODE_WAIT_REG_MEM                    0x3C
+#define PM4_HDR_IT_OPCODE_COPY_DATA                       0x40
+#define PM4_HDR_IT_OPCODE_DMA_DATA                        0x50
 
 #define PM4_HDR_SHADER_TYPE(x)                            (((x) & 0x1) << 1)
 #define PM4_HDR_IT_OPCODE(x)                              (((x) & 0xFF) << 8)
@@ -81,5 +89,52 @@
 
 #define PM4_RELEASE_MEM_DW1_EVENT_INDEX(x)                 (((x) & 0xF) << 8)
 #  define PM4_RELEASE_MEM_EVENT_INDEX_AQL                  0x7
+
+#define PM4_ATOMIC_MEM_DW1_ATOMIC(x)                       (((x) & 0x7F) << 0)
+#  define PM4_ATOMIC_MEM_GL2_OP_ATOMIC_SWAP_RTN_64         (39 << 0)
+#define PM4_ATOMIC_MEM_DW2_ADDR_LO(x)                      (((x) & 0xFFFFFFF8) << 0)
+#define PM4_ATOMIC_MEM_DW3_ADDR_HI(x)                      (((x) & 0xFFFFFFFF) << 0)
+#define PM4_ATOMIC_MEM_DW4_SRC_DATA_LO(x)                  (((x) & 0xFFFFFFFF) << 0)
+#define PM4_ATOMIC_MEM_DW5_SRC_DATA_HI(x)                  (((x) & 0xFFFFFFFF) << 0)
+
+#define PM4_COPY_DATA_DW1(x)                               (((x) & 0xFFFFFFFF) << 0)
+#  define PM4_COPY_DATA_SRC_SEL_ATOMIC_RETURN_DATA         (6 << 0)
+#  define PM4_COPY_DATA_DST_SEL_TC_12                      (2 << 8)
+#  define PM4_COPY_DATA_COUNT_SEL                          (1 << 16)
+#  define PM4_COPY_DATA_WR_CONFIRM                         (1 << 20)
+#define PM4_COPY_DATA_DW4_DST_ADDR_LO(x)                   (((x) & 0xFFFFFFF8) << 0)
+#define PM4_COPY_DATA_DW5_DST_ADDR_HI(x)                   (((x) & 0xFFFFFFFF) << 0)
+
+#define PM4_WAIT_REG_MEM_DW1(x)                            (((x) & 0xFFFFFFFF) << 0)
+#  define PM4_WAIT_REG_MEM_FUNCTION_EQUAL_TO_REFERENCE     (3 << 0)
+#  define PM4_WAIT_REG_MEM_MEM_SPACE_MEMORY_SPACE          (1 << 4)
+#  define PM4_WAIT_REG_MEM_OPERATION_WAIT_REG_MEM          (0 << 6)
+#define PM4_WAIT_REG_MEM_DW2_MEM_POLL_ADDR_LO(x)           (((x) & 0xFFFFFFFC) << 0)
+#define PM4_WAIT_REG_MEM_DW3_MEM_POLL_ADDR_HI(x)           (((x) & 0xFFFFFFFF) << 0)
+#define PM4_WAIT_REG_MEM_DW4_REFERENCE(x)                  (((x) & 0xFFFFFFFF) << 0)
+#define PM4_WAIT_REG_MEM_DW6(x)                            (((x) & 0x8000FFFF) << 0)
+#  define PM4_WAIT_REG_MEM_POLL_INTERVAL(x)                (((x) & 0xFFFF) << 0)
+#  define PM4_WAIT_REG_MEM_OPTIMIZE_ACE_OFFLOAD_MODE       (1 << 31)
+
+#define PM4_DMA_DATA_DW1(x)                            (((x) & 0xFFFFFFFF) << 0)
+#  define PM4_DMA_DATA_DST_SEL_DST_ADDR_USING_L2       (3 << 20)
+#  define PM4_DMA_DATA_SRC_SEL_SRC_ADDR_USING_L2       (3 << 29)
+#define PM4_DMA_DATA_DW2_SRC_ADDR_LO(x)                (((x) & 0xFFFFFFFF) << 0)
+#define PM4_DMA_DATA_DW3_SRC_ADDR_HI(x)                (((x) & 0xFFFFFFFF) << 0)
+#define PM4_DMA_DATA_DW4_DST_ADDR_LO(x)                (((x) & 0xFFFFFFFF) << 0)
+#define PM4_DMA_DATA_DW5_DST_ADDR_HI(x)                (((x) & 0xFFFFFFFF) << 0)
+#define PM4_DMA_DATA_DW6(x)                            (((x) & 0xFFFFFFFF) << 0)
+#  define PM4_DMA_DATA_BYTE_COUNT(x)                   (((x) & 0x3FFFFFF) << 0)
+#  define PM4_DMA_DATA_DIS_WC                          (1 << 31)
+#  define PM4_DMA_DATA_DIS_WC_LAST                     (0 << 31)
+
+#define PM4_WRITE_DATA_DW1(x)                          (((x) & 0xFFFFFF00) << 0)
+#  define PM4_WRITE_DATA_DST_SEL_TC_L2                 (2 << 8)
+#  define PM4_WRITE_DATA_WR_CONFIRM_WAIT_CONFIRMATION  (1 << 20)
+#define PM4_WRITE_DATA_DW2_DST_MEM_ADDR_LO(x)          (((x) & 0xFFFFFFFC) << 0)
+#define PM4_WRITE_DATA_DW3_DST_MEM_ADDR_HI(x)          (((x) & 0xFFFFFFFF) << 0)
+#define PM4_WRITE_DATA_DW4_DATA(x)                     (((x) & 0xFFFFFFFF) << 0)
+
+// clang-format on
 
 #endif  // header guard
