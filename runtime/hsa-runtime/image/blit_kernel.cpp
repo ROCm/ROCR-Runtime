af2cf991577d702d8267f63ff210ce2363053b52
Sreekant Somasekharan <sreekant.somasekharan@amd.com>
2023-08-30 21:46:16 -0400

    Initial GFX1201 changes.
    Add target gfx1201 to several files.
    
    Change-Id: I5cae7dba00ed58f8fbfa6e7147275bd7d5feaed0
    Signed-off-by: Sreekant Somasekharan <sreekant.somasekharan@amd.com>
    Signed-off-by: Chris Freehill <cfreehil@amd.com>
    
    
    [ROCm/ROCR-Runtime commit: 24463635f9559e3e1ba4f604ad114e887b4c8856]

--- runtime/hsa-runtime/image/blit_kernel.cpp
+++ runtime/hsa-runtime/image/blit_kernel.cpp
@@ -106,6 +106,7 @@ extern uint8_t ocl_blit_object_gfx1103[];
 extern uint8_t ocl_blit_object_gfx1150[];
 extern uint8_t ocl_blit_object_gfx1151[];
 extern uint8_t ocl_blit_object_gfx1200[];
+extern uint8_t ocl_blit_object_gfx1201[];
 
 // Arguments inserted by OCL compiler, all zero here.
 struct OCLHiddenArgs {
@@ -1055,6 +1056,8 @@ hsa_status_t BlitKernel::GetPatchedBlitObject(const char* agent_name,
     *blit_code_object = ocl_blit_object_gfx1151;
   } else if (sname == "gfx1200") {
     *blit_code_object = ocl_blit_object_gfx1200;
+  } else if (sname == "gfx1201") {
+    *blit_code_object = ocl_blit_object_gfx1201;
   } else {
     return HSA_STATUS_ERROR_INVALID_ISA_NAME;
   }
