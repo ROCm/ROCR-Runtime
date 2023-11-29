13d4c19d7c2b913e9927a216c82a2a3659f8c424
David Yat Sin <David.YatSin@amd.com>
2023-11-29 19:13:33 +0000

    PC Sampling: Implement flush
    Flush is used by the client to retrieve data that are currently stored
    in the buffers. This is used by the client to retrieve current data when
    the buffers are not full.
    
    Change-Id: Ib8304dcdfb2797cb060ec72df4970d95cf6be348
    
    
    [ROCm/ROCR-Runtime commit: 8abbf9475b6809d4dce303651331e0e6cda4e18c]

--- runtime/hsa-runtime/core/inc/amd_gpu_agent.h
+++ runtime/hsa-runtime/core/inc/amd_gpu_agent.h
@@ -734,6 +734,7 @@ class GpuAgent : public GpuAgentInt {
     uint8_t* host_buffer_wrap_pos;
     uint8_t* host_write_ptr;
     uint8_t* host_read_ptr;
+    std::mutex host_buffer_mutex;
 
     uint32_t which_buffer;
     uint64_t* old_val;
