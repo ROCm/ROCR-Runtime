f4b17c6b3622dc38e8892b23093f6e0f6ff2378f
David Yat Sin <David.YatSin@amd.com>
2024-08-22 15:43:16 +0000

    Set ELF_GETSHDRSTRNDX when cxx compiler is not loaded
    Change-Id: Ia26b8999909f688ce78d9bbe4cb2a7262df2ee02
    
    
    [ROCm/ROCR-Runtime commit: cb672ebcd145c1ca03972efb0682688a13c68624]

--- runtime/hsa-runtime/cmake_modules/FindLibElf.cmake
+++ runtime/hsa-runtime/cmake_modules/FindLibElf.cmake
@@ -51,14 +51,18 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibElf DEFAULT_MSG
   LIBELF_INCLUDE_DIRS)
 
 SET(CMAKE_REQUIRED_LIBRARIES elf)
-INCLUDE(CheckCXXSourceCompiles)
-CHECK_CXX_SOURCE_COMPILES("#include <libelf.h>
-int main() {
-  Elf *e = (Elf*)0;
-  size_t sz;
-  elf_getshdrstrndx(e, &sz);
-  return 0;
-}" ELF_GETSHDRSTRNDX)
+if (CMAKE_CXX_COMPILER_LOADED)
+  INCLUDE(CheckCXXSourceCompiles)
+  CHECK_CXX_SOURCE_COMPILES("#include <libelf.h>
+  int main() {
+    Elf *e = (Elf*)0;
+    size_t sz;
+    elf_getshdrstrndx(e, &sz);
+    return 0;
+  }" ELF_GETSHDRSTRNDX)
+else()
+set ( ELF_GETSHDRSTRNDX "TRUE" )
+endif(CMAKE_CXX_COMPILER_LOADED)
 
 mark_as_advanced(LIBELF_INCLUDE_DIRS LIBELF_LIBRARIES ELF_GETSHDRSTRNDX)
 
