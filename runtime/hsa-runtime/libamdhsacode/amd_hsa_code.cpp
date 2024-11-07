2ea3afc6e65ce08f138e1a77069c9cc4279db95d
Konstantin Zhuravlyov <kzhuravl_dev@outlook.com>
2024-11-07 10:57:50 -0500

    loader: add gfx12-generic support
    Change-Id: I0bf5d48ec357278bdb7a9c4eae61a7b7995411f0
    
    
    [ROCm/ROCR-Runtime commit: ec3d4aa5e95bb0bd6c9366cb98fe697c8697c493]

--- runtime/hsa-runtime/libamdhsacode/amd_hsa_code.cpp
+++ runtime/hsa-runtime/libamdhsacode/amd_hsa_code.cpp
@@ -613,6 +613,7 @@ namespace code {
       case ELF::EF_AMDGPU_MACH_AMDGCN_GFX10_1_GENERIC: MI.Name = "gfx10-1-generic"; MI.XnackSupported = true; MI.SrameccSupported = false; break;
       case ELF::EF_AMDGPU_MACH_AMDGCN_GFX10_3_GENERIC: MI.Name = "gfx10-3-generic"; MI.XnackSupported = false; MI.SrameccSupported = false; break;
       case ELF::EF_AMDGPU_MACH_AMDGCN_GFX11_GENERIC:   MI.Name = "gfx11-generic";   MI.XnackSupported = false; MI.SrameccSupported = false; break;
+      case ELF::EF_AMDGPU_MACH_AMDGCN_GFX12_GENERIC:   MI.Name = "gfx12-generic";   MI.XnackSupported = false; MI.SrameccSupported = false; break;
       default: return false;
       }
       return true;
