9ad8e2698be2c61655713ffb2b3dd91cce1e0e6c
Rahul Manocha <153310294+manocharahul@users.noreply.github.com>
2026-07-07 04:32:56 +0000

    ROCM-27353 - Disable WGP mode check for cu mask for gfx1250  (#8038)
    ## Motivation
    
    1. WGP mode is disabled for gfx1250.
    2. rocr should skip the cu mask check for gfx1250 to ensure invidual CUs
    can be masked.
    3. KFD also needs to extend support for this through
    AMDKFD_IOC_SET_CU_MASK ioctl
    
    ## Technical Details
    
    <!-- Explain the changes along with any relevant GitHub links. -->
    
    ## JIRA ID
    
    <!-- If applicable, mention the JIRA ID resolved by this PR (Example:
    Resolves SWDEV-12345). -->
    <!-- Do not post any JIRA links here. -->
    
    ## Test Plan
    
    <!-- Explain any relevant testing done to verify this PR. -->
    
    ## Test Result
    
    <!-- Briefly summarize test outcomes. -->
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    Co-authored-by: Rahul Manocha <rmanocha@amd.com>
    [rocm-systems] ROCm/rocm-systems#8038 (commit 88b4e43)

--- runtime/hsa-runtime/core/runtime/amd_aql_queue.cpp
+++ runtime/hsa-runtime/core/runtime/amd_aql_queue.cpp
@@ -1412,7 +1412,10 @@ hsa_status_t AqlQueue::SetCUMasking(uint32_t num_cu_mask_count, const uint32_t* 
   if ((!cu_mask_.empty()) || (num_cu_mask_count != 0) || (!global_mask.empty())) {
 
     // Devices with WGPs must conform to even-indexed contiguous pairwise CU enablement.
-    if (agent_->supported_isas()[0]->GetMajorVersion() >= 10) {
+    // Disable WGP mode check for gfx1250
+    if (agent_->supported_isas()[0]->GetMajorVersion() >= 10 &&
+        !(agent_->supported_isas()[0]->GetMajorVersion() == 12 &&
+          agent_->supported_isas()[0]->GetMinorVersion() >= 5)) {
       for (int i = 0; i < mask.size() * 32; i += 2) {
         uint32_t cu_pair = (mask[i / 32] >> (i % 32)) & 0x3;
         if (cu_pair && cu_pair != 0x3) return HSA_STATUS_ERROR_INVALID_ARGUMENT;
