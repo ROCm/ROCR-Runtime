59fbc92637a72ccebf5fa56ffa3fa2da2878c832
xiaogang-chen-amd <184869709+xiaogang-chen-amd@users.noreply.github.com>
2026-09-02 23:32:28 +0000

    libhsakmt: not fallback to old allocation mechanism when use  udmabuf
    if HSA_USE_UDMABUF=1 is used and there is not enough udmabuf memory
    not falback to old allocation mechanism as customer require.
    
    Signed-off-by: Xiaogang Chen<xiaogang.chen@amd.com>
    [rocm-systems] ROCm/rocm-systems#11033 (commit 72d1c79)

--- libhsakmt/src/fmm.c
+++ libhsakmt/src/fmm.c
@@ -1842,11 +1842,14 @@ void *hsakmt_fmm_allocate_device(HsaKFDContext *ctx,
 	if (hsakmt_udmabuf_dev_fd > 0 && aperture == fmm_ctx->svm.dgpu_aperture
 		 && hsakmt_device_is_apu_by_node_id(ctx, node_id)
 		 && aperture->ops == &mmap_aperture_ops) {
+
 		mem  = udmabuf_allocation(ctx, gpu_id, node_id, size, aperture, alignment,
                                         mflags, &vm_obj);
 		pr_debug("udmabuf_allocation mem %p\n", mem);
-		if (!mem)
-			pr_debug("udmabuf_allocation allocation fail\n");
+		if (!mem) {
+			pr_err("udmabuf_allocation allocation fail size %lu\n", size);
+			return NULL;
+		}
 	}
 
 	/* env HSA_USE_UDMABUF not set, or not apu, or cannot use udmabuf,
